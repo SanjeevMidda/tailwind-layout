da665a8dd7b6d52af389c7f203e8258d136fadd4
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2023-07-04 10:37:17 +0100

    Add files via upload
    Uploading source files.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <h1 className="">TAILWIND CSS</h1>
+      <p className="text-center w-1/2 p-10">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+        obcaecati voluptatum rem voluptas excepturi. Dolore quam ad pariatur
+        eaque consectetur esse provident expedita perspiciatis? Facilis
+        consequuntur soluta similique perspiciatis debitis?
+      </p>
+    </div>
+  );
+}
+
+export default App;
